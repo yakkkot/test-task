712780176d037e22f853324c946a483ef53003d4
yakkkot <k0tjeee777@gmail.com>
2022-11-20 22:28:50 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,6 @@ import {ListItems,Button,Holidays} from "./Components";
 
 const App = () => {
 
-// ToDo:
-// #1 Display full list of items fethced from the API.
-// #2 Filter fetched items based on 'Search text' input.
-// #3 By clicking on country from the 'Search text' need to fetch and display holidays(selectedCountryHolidays) in the selected county.
-// Endpoint for holidays: https://date.nager.at/api/v3/NextPublicHolidays/{countryCode}
-// #4 Add a Sort button next to the input. It should sort the list of countries that the user sees on the screen in desc or asc order.
-// Default order os desc. The name of the button should indicate what type of sorting will be performed when clicked.
-// #5 Next to the Sort button, add a Reset button to the empty holidays
-// * You are welcome to edit/refactor any piece of code below if you believe it can be improved or to express your code style.
-
     const [countries, setCountries] = useState([]);
     const [sortAZ, setSortAZ] = useState(false);
     const [search, setSearch] = useState('');
